85393ce16ac9f0b849faf9e6e7fc3eded72dac09
sb2bg <sullivanbognar@gmail.com>
2021-11-24 18:22:34 -0600

    Add form and errors

--- pages/anagrams.tsx
+++ pages/anagrams.tsx
@@ -1,8 +1,61 @@
 import Head from "next/head";
+import React, { useState } from "react";
 import AnagramLetters from "../components/AnagramLetters";
+import Banner from "../components/Banner";
 import styles from "../styles/Index.module.css";
 
 const WordHunt = () => {
+  const [letters, setLetters] = useState(new Array(6).fill(" "));
+  const [words, setWords] = useState([]);
+  const [show, setShow] = useState(false);
+
+  const handleChange = (e: any) => {
+    const newLetters = [];
+
+    for (const letter of e.target.value) {
+      if (letter.match(/[a-z]/i)) {
+        newLetters.push(letter.toUpperCase());
+      }
+    }
+
+    for (let i = newLetters.length; i < 6; i++) {
+      newLetters.push(" ");
+    }
+
+    setLetters(newLetters);
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+
+    const joined = letters.join("");
+
+    if (joined.length < 6 || !joined.match(/[a-z]/i)) {
+      setShow(true);
+
+      setTimeout(() => {
+        setShow(false);
+      }, 3200);
+
+      return;
+    }
+
+    const url =
+      process.env.NODE_ENV === "production"
+        ? "https://gamepigeon-solver.vercel.app"
+        : "http://localhost:3000";
+
+    fetch(`${url}/api/anagrams/${letters.join("")}`)
+      .then((data) => data.json())
+      .then((json) => {
+        setWords([]);
+
+        setWords(
+          json.words.sort((a: string, b: string) => b.length - a.length)
+        );
+      });
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,9 +67,35 @@ const WordHunt = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>Anagrams Solver</h1>
 
-        <AnagramLetters />
+        <AnagramLetters letters={letters} />
+        <Banner showing={show}>
+          To submit, please make sure you have 6 alphabetic characters
+        </Banner>
 
-        <p className={styles.description}>Word Combinations</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            autoCorrect="off"
+            autoCapitalize="off"
+            spellCheck={false}
+            className={styles.input}
+            maxLength={6}
+            onChange={handleChange}
+            type="text"
+          />
+          <button className={styles.submit} type="submit">
+            Solve
+          </button>
+        </form>
+
+        <p className={styles.subtitle}>Word Combinations</p>
+
+        <div className={styles["word-grid"]}>
+          {words.map((word) => (
+            <div key={word} className={styles.word}>
+              {word}
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
