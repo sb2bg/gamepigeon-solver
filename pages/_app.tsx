ed7fb196f11d7d4f54b6f3b5215c9cd27976dfe0
sb2bg <sullivanbognar@gmail.com>
2021-11-28 13:43:33 -0600

    Add a (currently broken) back button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
+import styles from "../styles/Index.module.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Link href="/">
+        <a className={styles.home}>&larr; Home</a>
+      </Link>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default MyApp;
