eb48cb8db7653c4ebc544345573d79423cd786a9
sb2bg <sullivanbognar@gmail.com>
2023-12-26 04:35:33 -0600

    Add onClick event to WordGrid items

--- components/WordGrid.tsx
+++ components/WordGrid.tsx
@@ -2,10 +2,11 @@ import styles from "../styles/WordGrid.module.css";
 
 interface WordGridProps {
   words: string[];
+  onClick?: (word: string) => void;
   loading: boolean;
 }
 
-const WordGrid: React.FC<WordGridProps> = ({words, loading}) => {
+const WordGrid: React.FC<WordGridProps> = ({words, onClick, loading}) => {
   return loading ? (
     <div>Loading...</div>
   ) : words.length === 0 ? (
@@ -13,7 +14,11 @@ const WordGrid: React.FC<WordGridProps> = ({words, loading}) => {
   ) : (
     <div className={styles.grid}>
       {words.map((word) => (
-        <div key={word} className={styles.item}>
+        <div
+          key={word}
+          className={styles.item}
+          onClick={() => onClick && onClick(word)}
+        >
           {word}
         </div>
       ))}
