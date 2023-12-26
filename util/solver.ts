f772455b01374cf5cd7c80550ce89bb23a1a74a9
sb2bg <sullivanbognar@gmail.com>
2023-12-26 01:26:43 -0600

    Add word-hunt solver functionality

--- util/solver.ts
+++ util/solver.ts
@@ -1,3 +1,5 @@
+import {Grid} from "../pages/word-hunt";
+
 const getWords = async (): Promise<Set<string>> => {
   return new Set(
     await fetch("words.txt")
@@ -6,9 +8,11 @@ const getWords = async (): Promise<Set<string>> => {
   );
 };
 
-export const solveAnagrams = async (letters: string): Promise<string[]> => {
-  const words = await getWords();
+const dict = getWords();
+
+export const solveAnagrams = async (letters: string) => {
   const result: Set<string> = new Set();
+  const words = await dict;
 
   const recursiveHelper = async (letters: string, curr: string) => {
     if (words.has(curr.toUpperCase())) {
@@ -29,6 +33,49 @@ export const solveAnagrams = async (letters: string): Promise<string[]> => {
 
   for (let i = 0; i < letters.length; i++) {
     recursiveHelper(letters.slice(0, i) + letters.slice(i + 1), letters[i]);
+  }
+
+  return new Array(...result);
+};
+
+export const solveWordHunt = async (grid: Grid) => {
+  const words = await dict;
+
+  const rows = grid.length;
+  const cols = grid[0].length;
+  const result: Set<string> = new Set();
+  const visited: boolean[][] = Array.from({length: rows}, () =>
+    Array(cols).fill(false)
+  );
+
+  const dfs = (row: number, col: number, path: string) => {
+    if (row < 0 || col < 0 || row >= rows || col >= cols || visited[row][col])
+      return;
+
+    path += grid[row][col];
+
+    if (path.length >= 3 && words.has(path.toUpperCase())) {
+      result.add(path);
+    }
+
+    visited[row][col] = true;
+
+    dfs(row + 1, col, path);
+    dfs(row - 1, col, path);
+    dfs(row, col + 1, path);
+    dfs(row, col - 1, path);
+    dfs(row + 1, col + 1, path);
+    dfs(row + 1, col - 1, path);
+    dfs(row - 1, col + 1, path);
+    dfs(row - 1, col - 1, path);
+
+    visited[row][col] = false;
+  };
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      dfs(row, col, "");
+    }
   }
 
   return new Array(...result);
