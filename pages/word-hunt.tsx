245086e236cc668eedf35e7a2634510ad325473e
sb2bg <sullivanbognar@gmail.com>
2023-12-26 04:35:44 -0600

    Add useState and useEffect hooks, update state variables

--- pages/word-hunt.tsx
+++ pages/word-hunt.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import styles from "../styles/Index.module.css";
 import LetterInputGrid from "../components/LetterInputGrid";
 import WordGrid from "../components/WordGrid";
@@ -8,7 +8,11 @@ import {solveWordHunt} from "../util/solver";
 export type Grid = string[][];
 
 const WordHunt = () => {
-  const [words, setWords] = useState<string[]>([]);
+  const [words, setWords] = useState<Map<string, [number, number][]>>(
+    new Map()
+  );
+  const [highlighted, setHighlighted] = useState<[number, number][]>([]);
+  const [colors, setColors] = useState<Map<string, string>>(new Map());
   const [loading, setLoading] = useState(false);
   const [lastQuery, setLastQuery] = useState<Grid>(
     Array(4)
@@ -24,12 +28,24 @@ const WordHunt = () => {
     setLoading(true);
 
     solveWordHunt(words).then((solved) => {
-      setWords(solved.sort((a: string, b: string) => b.length - a.length));
+      setWords(solved);
       setLoading(false);
     });
 
     setLastQuery(words);
   };
+
+  useEffect(() => {
+    const newColors = new Map<string, string>();
+    const curr = [255, 0, 0];
+
+    highlighted.forEach((coords) => {
+      newColors.set(`${coords[0]},${coords[1]}`, `rgb(${curr.join(",")})`);
+      curr[1] += 30;
+    });
+
+    setColors(newColors);
+  }, [highlighted]);
 
   return (
     <div className={styles.container}>
@@ -39,9 +55,15 @@ const WordHunt = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Word Hunt Solver</h1>
-        <LetterInputGrid onSubmit={handleSubmit} />
+        <LetterInputGrid onSubmit={handleSubmit} colors={colors} />
         <p className={styles.subtitle}>Word Combinations</p>
-        <WordGrid words={words} loading={loading} />
+        <WordGrid
+          onClick={(word) => {
+            setHighlighted(words.get(word) || []);
+          }}
+          words={Array.from(words.keys()).sort((a, b) => b.length - a.length)}
+          loading={loading}
+        />
       </main>
     </div>
   );
