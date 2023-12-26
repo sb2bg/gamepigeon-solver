1260924c59b27e9fc708db0ef4170d837af1c733
sb2bg <sullivanbognar@gmail.com>
2023-12-26 01:26:02 -0600

    Refactor AnagramLetters component

--- components/AnagramLetters.tsx
+++ components/AnagramLetters.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import {useState} from "react";
 import styles from "../styles/AnagramLetters.module.css";
 import LetterInputField from "./LetterInputField";
+import Head from "next/head";
 
 interface AnagramLettersProps {
   onSubmit: (letters: string[]) => void;
 }
 
-const AnagramLetters: React.FC<AnagramLettersProps> = ({ onSubmit }) => {
+const AnagramLetters: React.FC<AnagramLettersProps> = ({onSubmit}) => {
   const [letters, setLetters] = useState<string[]>(new Array(6).fill(""));
   const [focused, setFocused] = useState<number>(0);
 
@@ -19,30 +20,37 @@ const AnagramLetters: React.FC<AnagramLettersProps> = ({ onSubmit }) => {
   };
 
   return (
-    <div>
-      <div className={styles.letters}>
-        {letters.map((letter, index) => {
-          return (
-            <div key={index}>
-              <LetterInputField
-                letter={letter}
-                focused={focused === index}
-                setFocus={(_, j) => {
-                  if (j >= 0 && j < letters.length) {
-                    setFocused(j);
-                  }
-                }}
-                setLetter={(letter) => {
-                  const newLetters = [...letters];
-                  newLetters[index] = letter;
-                  setLetters(newLetters);
-                }}
-                index={[0, index]}
-                onSubmit={handleSubmit}
-              />
-            </div>
-          );
-        })}
+    <div className={styles.container}>
+      <Head>
+        <title>Anagram Solver</title>
+      </Head>
+
+      <div>
+        <div className={styles.letters}>
+          {letters.map((letter, index) => {
+            return (
+              <div key={index}>
+                <LetterInputField
+                  type="anagram"
+                  letter={letter}
+                  focused={focused === index}
+                  setFocus={(_, j) => {
+                    if (j >= 0 && j < letters.length) {
+                      setFocused(j);
+                    }
+                  }}
+                  setLetter={(letter) => {
+                    const newLetters = [...letters];
+                    newLetters[index] = letter;
+                    setLetters(newLetters);
+                  }}
+                  index={[0, index]}
+                  onSubmit={handleSubmit}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
